568dfa25675a7437cbe10aacebdd020b91075940
erltho <erlendthorsrud@gmail.com>
2021-09-07 18:25:55 +0200

    Update ubuntu_setup_script.sh
    update docker-compose

--- ubuntu_setup_script.sh
+++ ubuntu_setup_script.sh
@@ -27,7 +27,7 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io
 
 echo "installing docker-compose..."
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
 echo "installing go...."
